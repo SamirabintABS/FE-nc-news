e7b780db2a1d26bb1e226b0421001e65c197d5f2
SamirabintABS <68996388+SamirabintABS@users.noreply.github.com>
2023-04-18 15:00:48 +0100

    tweak styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,14 @@
 const Header = () => {
     return (
-        <h1 className="app-header">Your Daily Ribbit</h1>
+        <div className="frog-header">
+            <a href="https://emoji.gg/emoji/7522-purple-froggie">
+                <img src="https://cdn3.emoji.gg/emojis/7522-purple-froggie.png" width="48px" height="48px" alt="purple_froggie" />
+            </a>
+            <h1 className="app-header">Your Daily Ribbit</h1>
+            <a href="https://emoji.gg/emoji/7522-purple-froggie">
+                <img src="https://cdn3.emoji.gg/emojis/7522-purple-froggie.png" width="48px" height="48px" alt="purple_froggie" />
+            </a>
+        </div>
     )
 }
 
