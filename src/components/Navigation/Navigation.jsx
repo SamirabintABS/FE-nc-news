0c7c5bf85e63f4d7e2071307e7b77ba3e1f0f2b8
SamirabintABS <68996388+SamirabintABS@users.noreply.github.com>
2023-04-18 10:57:17 +0100

    add profil svg

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,4 +1,3 @@
-import SelectTopic from "./SelectTopic";
 import ncLogo from '../../images/Nc (1)-PhotoRoom (1).png'
 
 const Navigation = () => {
@@ -8,7 +7,8 @@ const Navigation = () => {
                 <img src={ncLogo} alt="nc news logo" />
             </div>
             <div className="user-container">
-                <button>User</button>
+                <button> <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none" /><path d="M3 5v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H5c-1.11 0-2 .9-2 2zm12 4c0 1.66-1.34 3-3 3s-3-1.34-3-3 1.34-3 3-3 3 1.34 3 3zm-9 8c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1H6v-1z" /></svg></button>
+
             </div>
         </div>
     )
