dca1c3b849728e729ae29c93b89c142bf21280ce
SamirabintABS <68996388+SamirabintABS@users.noreply.github.com>
2023-04-18 11:55:42 +0100

    add Header and other component files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 import Navigation from './components/Navigation/Navigation.jsx';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Navigation />} />
+        <Route path="/" element={
+          <>
+            <Navigation />
+            <Header />
+          </>
+        } />
       </Routes>
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return (
+        <h1 className="app-header">Your Daily Ribbit</h1>
+    )
+}
+
+export default Header;
